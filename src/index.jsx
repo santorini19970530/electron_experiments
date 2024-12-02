9d25ac39869ebe6c49899b12205148a1420b2372
santorini19970530 <dennischngai@gmail.com>
2024-12-02 21:57:45 +0800

    add REACT first, then add Tailwind CSS into Electron, all things redo

---
+++ src/index.jsx
@@ -0,0 +1,9 @@
+// source of this project https://www.youtube.com/watch?v=SiMIBb20S7c
+
+import * as React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+
+const root = createRoot(document.body);
+root.render(<App />);
